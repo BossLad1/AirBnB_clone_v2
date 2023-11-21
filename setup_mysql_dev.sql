a9b488ced6a7b22722d7b9a472c007fade787ec3
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-11-21 08:52:59 -0800

    AirBnB clone

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,9 @@ CREATE DATABASE IF NOT EXIST hbnb_dev_db;
 -- creating new user named
 -- with the password
 CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIES BY 'hbnb_dev_pwd';
-
+-- granting all privileges to the new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+-- granting the SELECT privilege for the user hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
